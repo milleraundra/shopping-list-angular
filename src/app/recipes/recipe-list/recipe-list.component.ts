e74e5c9227920a74fd69cdde233bb92323cdb41e
milleraundra <miller.aundra@gmail.com>
2017-05-10 10:41:24 -0700

    add first pass to styled items and ngFor to repeat list items

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Spring Rolls', 'Delicious recipe from spring rolls', 'https://s-media-cache-ak0.pinimg.com/736x/18/6e/6a/186e6aaacc35a81699b538868d8b925b.jpg'),
+    new Recipe('California Roll', 'Simple and scrumptious', 'https://www.sushihaven.co.uk/media/catalog/product/cache/1/image/650x/040ec09b1e35df139433887a97daa66f/c/a/california_roll.jpg')
+  ];
 
   constructor() { }
 
