2e953468e05d28ec92306a58ca0a2ffb80d64c5d
Aundra Miller <miller.aundra@gmail.com>
2017-06-02 15:34:23 -0700

    display selected recipe from list in recipe-detail component

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,5 @@
 import {Component, EventEmitter, OnInit, Output, Input} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -6,7 +7,7 @@ import {Component, EventEmitter, OnInit, Output, Input} from '@angular/core';
   styleUrls: ['recipe-detail.component.css']
 })
 export class RecipeDetailComponent implements OnInit {
-  // @Input() propertyName = 'thing';
+  @Input() recipe: Recipe;
   // @Output() eventName = new EventEmitter<{name: 'string', value: {}}>();
 
   constructor() { }
